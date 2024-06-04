ccfce550f9d407ccda62581f168f9022f92243a0
jonatas <jonatassantos96@hotmail.com>
2024-06-04 16:46:58 -0300

    ✨ Create colors file

---
+++ src/theme/colors.ts
@@ -0,0 +1,10 @@
+export const themeColors = {
+  highlightedDateColors: {
+    warningTextColor: '#846501fc',
+    warningBackgroundColor: '#edd690',
+    dangerTextColor: '#ad000dd9',
+    dangerbackgroundColor: '#f3afb4',
+    successTextColor: '#09721b',
+    successBackgroundColor: '#c8e5d0',
+  },
+};
